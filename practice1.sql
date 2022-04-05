980ceb5338436e23fcf88d4bbbae1352638d38db
Victor Bustillos <68461123+cheveuxdelin@users.noreply.github.com>
2022-04-05 09:24:31 -0500

    Create practice1.sql

---
+++ practice1.sql
@@ -0,0 +1,8 @@
+-- part 1
+SELECT school, first_name FROM teachers ORDER BY school ASC, first_name ASC;
+
+-- part 2
+SELECT * FROM teachers WHERE first_name LIKE 'S%' AND salary > 40000;
+
+-- part 3
+SELECT * FROM teachers WHERE hire_date > '2010-01-01' ORDER BY salary DESC;
